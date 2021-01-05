0697e8d6510387cdc4d31492a5170499038c97ed
John B <jbethancourt.ctr@revacomm.com>
2021-01-05 10:22:56 -1000

    Pretify

--- PBLogging/Extensions/Color+PBLStyle.swift
+++ PBLogging/Extensions/Color+PBLStyle.swift
@@ -8,11 +8,11 @@ import SwiftUI
 
 extension Color {
     
-    public static let pblSlate =    Color("slate")
-    public static let pblFog =      Color("fog")
-    public static let pblHaze =     Color("haze")
-    public static let pblMist =     Color("mist")
-    public static let pblNotBlack = Color("notBlack")
-    public static let pblGray =     Color("PBLGray")
+    public static let pblSlate      = Color("slate")
+    public static let pblFog        = Color("fog")
+    public static let pblHaze       = Color("haze")
+    public static let pblMist       = Color("mist")
+    public static let pblNotBlack   = Color("notBlack")
+    public static let pblGray       = Color("PBLGray")
     
 }
