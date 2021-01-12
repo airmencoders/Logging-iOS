58bd7f6117959efda7d463f41b5adc475974edaa
jbethanco <76016626+jbethanco@users.noreply.github.com>
2021-01-11 16:56:01 -1000

    Refactor Sidebar (#163)
    * Refactor Sidebar
    Add offset and padding animation to sidebar
    Add Animation extension file to store our animations
    Add UIDevice extension to make device rotation publisher
    Subscribe Sidebar to rotations and animate open sidebar when landscape
    
    * Add comments concerning Combine Publisher in UIDevice extension
    Add store and restore side bar collapsed preference upon rotation to match Apple NavigationView sidebar behavior
    Add comments to explain the above
    Expand SideViewButtons back to multiline
    Rotate the chevron button a full 180

---
+++ PBLogging/Extensions/Animation+Ext.swift
@@ -0,0 +1,16 @@
+//
+//  Animation+Ext.swift
+//  Logging
+//
+//  Created by John Bethancourt on 1/9/21.
+//
+
+import SwiftUI
+extension Animation {
+    public static func bounce() -> Animation {
+        return .interpolatingSpring(mass: 0.07, stiffness: 22, damping: 1, initialVelocity: 0.5)
+    }
+    public static func smooth() -> Animation {
+        return .easeInOut(duration: 0.3)
+    }
+}
---
+++ PBLogging/Extensions/UIDevice+Ext.swift
@@ -0,0 +1,18 @@
+//
+//  UIDevice+Ext.swift
+//  Logging
+//
+//  Created by John Bethancourt on 1/10/21.
+//
+
+import UIKit
+extension UIDevice{
+    
+    // A Combine Framework based Orientation Change Publisher that can be subscried to from anywhere.
+    // For more information on the Combine Framework, please see: https://developer.apple.com/wwdc19/722
+    static let orientationDidChangedPublisher = NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)
+        //"makeConnectable" makes it conform to the ConnectablePublisher protocol which exposes connect and autoconnect
+        .makeConnectable()
+        //"autoconnect" starts the flow of data as soon as a subscriber requests it.
+        .autoconnect()
+}
