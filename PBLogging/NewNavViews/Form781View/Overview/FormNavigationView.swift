05ed6970b2006260c2c5baf2296917bd513a81e5
Pete Misik <pmisik@revacomm.com>
2021-02-12 11:44:49 -0500

    Added delete function to app

--- PBLogging/NewNavViews/Form781View/Overview/FormNavigationView.swift
+++ PBLogging/NewNavViews/Form781View/Overview/FormNavigationView.swift
@@ -17,29 +17,57 @@ struct FormNavigationView: View {
     
     private var forms: FetchedResults<Form781>
     
+    init() {
+        if #available(iOS 14.0, *) {
+            
+        } else {
+            UITableView.appearance().tableFooterView = UIView()
+        }
+        
+        UITableView.appearance().separatorStyle = .none
+    }
+    
     var body: some View {
         NavigationView {
-            ScrollView {
-                VStack(spacing: 30) {
+//            ScrollView {
+            VStack(spacing: 30) {
+                List{
                     ForEach(forms, id: \.self) { form in
                         NavigationLink(
                             //destination: Main781FormView(form: form),
                             destination: FormTabView(form: form),
                             label: { Form781Card(form: form) })
+                            .padding()
                     }
+                    .onDelete(perform: deleteForm)
                 }
                 .padding()
             }
             .accessibility(identifier: "formsScrollView")
             .navigationBarTitle(Text("Mission Forms"))
             .navigationBarItems(trailing:
-                                    TextAndIconButton(text: "New Form", size: 24.0, icon: "plus.circle")
-                                    {
-                        PersistenceController.newRecordForContext()
-                                    }
-                                    .accessibility(identifier: "addFormButton"))
+                                    HStack {
+                                        TextAndIconButton(text: "New Form", size: 24.0, icon: "plus.circle")
+                                        {
+                                            PersistenceController.newRecordForContext()
+                                        }
+                                        .accessibility(identifier: "addFormButton")
+                                        EditButton()
+                                    })
         }
         .navigationViewStyle(StackNavigationViewStyle())
+    }
+    
+    func deleteForm(on offset: IndexSet) {
+        for index in offset {
+            let form = forms[index]
+            moc.delete(form)
+        }
+        do {
+            try moc.save()
+        } catch {
+            NSLog("CoreData Save error in deleteForm")
+        }
     }
 }
 
