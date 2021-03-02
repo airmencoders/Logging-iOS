988cac7bfda7eb3fa31c7128efcc984c46589fc9
Pete-Hoch <74692889+Pete-Hoch@users.noreply.github.com>
2021-03-01 14:23:35 -1000

    Expand testAllSortiesCalculatedTime to include multiple sorites. (#266)

--- LoggingTests/Event+CalculationsTests.swift
+++ LoggingTests/Event+CalculationsTests.swift
@@ -14,25 +14,41 @@ class Event_CalculationsTests: XCTestCase {
         let dataController = DataController(inMemory: true)
         let context =  dataController.container.viewContext
         let event = Event(context: context)
+        let secondsPerHour = 60.0 * 60.0
 
+        // Subtest 1 - Empty Sortie list.
         var calculatedTime = event.allSortiesCalculatedTime
         XCTAssertEqual(calculatedTime, "0.0")
 
+        // Subtest 2 - No takeoff or land time.
         let sortie = Sortie(context: context)
         sortie.event = event
         calculatedTime = event.allSortiesCalculatedTime
         XCTAssertEqual(calculatedTime, "")
 
-        sortie.takeoffTime = Date(timeIntervalSinceNow: 0.0)
+        // Subtest 3 - No land time.
+        sortie.takeoffTime = Date()
         calculatedTime = event.allSortiesCalculatedTime
         XCTAssertEqual(calculatedTime, "")
 
-        sortie.landTime = Date(timeIntervalSinceNow: 751.8)
+        // Subtest 4 - Part of an hour with round down.
+        sortie.landTime = Date(timeInterval: secondsPerHour * 0.21, since: sortie.takeoffTime!)
         calculatedTime = event.allSortiesCalculatedTime
         XCTAssertEqual(calculatedTime, "0.2")
 
-        sortie.landTime = Date(timeIntervalSinceNow: 4351.8)
+        // Subtest 6 - Part of an hour with round up.
+        sortie.landTime = Date(timeInterval: secondsPerHour * 1.19, since: sortie.takeoffTime!)
         calculatedTime = event.allSortiesCalculatedTime
         XCTAssertEqual(calculatedTime, "1.2")
+
+        // Subtest 7 - Add 4 more sorties for a total of 2 hours.
+        for _ in 1...4 {
+            let multiSorties = Sortie(context: context)
+            multiSorties.event = event
+            multiSorties.takeoffTime = Date()
+            multiSorties.landTime = Date(timeInterval: secondsPerHour * 0.21, since: multiSorties.takeoffTime!)
+        }
+        calculatedTime = event.allSortiesCalculatedTime
+        XCTAssertEqual(calculatedTime, "2.0")
     }
 }
