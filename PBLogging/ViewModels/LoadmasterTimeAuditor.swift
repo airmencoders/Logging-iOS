73b5a570aa6871b03ecc878fbaad4e2f39ec876b
jbethanco <76016626+jbethanco@users.noreply.github.com>
2021-03-05 15:20:51 -1000

    Sortie Flight Time Editing (#272)
    * Add ViewModel and Views for Splitting time
    
    * Shorten reduce
    
    * Replace Flight Time View with Auditing Flight Time View
    
    * Put AuditingFlightTimeView into scroll view so it works in landscape mode.

---
+++ PBLogging/ViewModels/LoadmasterTimeAuditor.swift
@@ -0,0 +1,144 @@
+//
+//  LoadmasterTimeAuditor.swift
+//  Logging
+//
+//  Created by John Bethancourt on 3/5/21.
+//
+
+import SwiftUI
+/// Provides a model for a SwiftUI view to work the flight time of a Sortie
+///
+/// It's essentially all value types and can be used as a @State var in a SwiftUI view
+/// The properties can then be bound to controls in the view.
+/// When values change, the state mutates and updates as described.
+struct LoadmasterTimeAuditor: Auditing {
+    var sortie: Sortie
+    var dutyPosition = DutyPositions.loadmaster
+    var totalFlightTime = 10.0
+    var totalFlightTimeString: String { "(\(String(format: "%1.1f", totalFlightTime)))" }
+    var rows: [TimeRow]
+    var canAudit = true
+    var numLoadmastersRequired: Int16
+    
+    /// Calculate sum of all row values using reduce
+    ///
+    /// https://developer.apple.com/documentation/swift/array/2298686-reduce
+    var totalRow: DisplayRow {
+        var total           = DisplayRow()
+        total.primary       = self.rows.reduce(0.0) { $0 + $1.primary.value }
+        total.secondary     = self.rows.reduce(0.0) { $0 + $1.secondary.value  }
+        total.instructor    = self.rows.reduce(0.0) { $0 + $1.instructor.value  }
+        total.evaluator     = self.rows.reduce(0.0) { $0 + $1.evaluator.value  }
+        total.other         = self.rows.reduce(0.0) { $0 + $1.other.value }
+        return total
+    }
+     
+    /// declare boolean errors if totals exceed total flight time
+    var primaryTotalHasProblem:     Bool { totalRow.primary.rounded(toPlaces: 1)     > totalFlightTime * Double(numLoadmastersRequired) }
+    var secondaryTotalHasProblem:   Bool { totalRow.secondary.rounded(toPlaces: 1)   > totalFlightTime }
+    var instructorTotalHasProblem:  Bool { totalRow.instructor.rounded(toPlaces: 1)  > totalFlightTime }
+    var evaluatorTotalHasProblem:   Bool { totalRow.evaluator.rounded(toPlaces: 1)   > totalFlightTime }
+    var otherTotalHasProblem:       Bool { totalRow.other.rounded(toPlaces: 1)      > totalFlightTime }
+      
+    /// If the errors above are true, assign a string to corresponding variables
+    var primaryTotalError: String? {
+        primaryTotalHasProblem ? "Primary time (\(totalRow.primary.displayString)) exceeds total flight time (\(totalFlightTime.displayString))." : nil
+    }
+    var secondaryTotalError: String? {
+        secondaryTotalHasProblem ? "Secondary time (\(totalRow.secondary.displayString)) exceeds total flight time (\(totalFlightTime.displayString))." : nil
+    }
+    var instructorTotalError: String? {
+        instructorTotalHasProblem ? "Instructor time (\(totalRow.instructor.displayString)) exceeds total flight time (\(totalFlightTime.displayString))." : nil
+    }
+    var evaluatorTotalError: String? {
+        evaluatorTotalHasProblem ? "Evaluator time (\(totalRow.evaluator.displayString)) exceeds total flight time (\(totalFlightTime.displayString))." : nil
+    }
+     
+    /// Make an array of existing errors using compactMap
+    ///
+    /// https://developer.apple.com/documentation/swift/sequence/2950916-compactmap
+    var errors: [String] {
+        let errors = [primaryTotalError, secondaryTotalError, instructorTotalError, evaluatorTotalError]
+        return errors.compactMap{ $0 }
+    }
+    
+    /// calculate the ghost time
+    var primaryGhost:   Double { (totalFlightTime - totalRow.primary).rounded(toPlaces: 1)  }
+    var secondaryGhost: Double { (totalFlightTime - totalRow.secondary) }
+    
+    var ghostRow: DisplayRow {
+        var ghost = DisplayRow()
+        ghost.primary = primaryGhost
+      
+        //prevent -0.0 from showing up
+        if ghost.primary == 0 && ghost.primary.sign == .minus{
+            ghost.primary = 0
+        }
+        ghost.secondary = secondaryGhost
+        return ghost
+    }
+    
+   
+    
+    init(with sortie: Sortie){
+        
+        var timeRows = [TimeRow]()
+        for case let line in sortie.crewLines where line.isLoadmaster == true {
+            let row = TimeRow(with: line)
+            timeRows.append(row)
+        }
+        self.rows = timeRows
+        if let sortieTime = sortie.calculatedTotalFlightTimeFor781{
+            self.totalFlightTime = sortieTime
+        }else {
+            self.canAudit = false
+        }
+        
+        self.numLoadmastersRequired = sortie.numLoadmastersRequired
+        self.sortie = sortie
+    }
+    
+    mutating func splitPrimaryEqually(){
+        print(#function)
+        guard canAudit == true else { return }
+        guard Int(totalFlightTime * 10.0) > rows.count else { return } // not enough time to split
+        print("passed guard")
+        
+        /// reset all values
+        for i in 0..<rows.count{
+            rows[i].primary.value     = 0
+            rows[i].secondary.value   = 0
+            rows[i].instructor.value  = 0
+            rows[i].evaluator.value   = 0
+            rows[i].other.value       = 0
+        }
+        
+        /// multiply the total flight time by 10 so we can do integer math
+        /// 1.1 becomes 11
+        let totalTime = Int(totalFlightTime * 10) * Int(numLoadmastersRequired)
+        /// integer math causes the equal time to remove any remainder
+        /// e.g. 13 / 3 will equal 4 instead of 4.33333 (4 remainder 1)
+        let equalTime = totalTime / rows.count
+        /// modulo tells us the remaining amount
+        var remainder = totalTime % equalTime
+        
+        for i in 0..<rows.count {
+            /// give each loadmaster their fair share and some of the remainder if any
+            rows[i].primary.value = Double(equalTime) / 10.0
+            if remainder > 0 {
+                rows[i].primary.value += 0.1
+                remainder -= 1
+            }
+        }
+      print(primaryGhost)
+    }
+    
+    func save(){
+        self.sortie.numLoadmastersRequired = self.numLoadmastersRequired
+        for row in self.rows{
+            row.save()
+        }
+    }
+}
+
+
---
+++ PBLogging/Views/TabViews/FlightTimeTab/AuditingFlightTimeView.swift
@@ -0,0 +1,157 @@
+//
+//  PilotFlightTimeView.swift
+//  Logging
+//
+//  Created by John Bethancourt on 2/26/21.
+//
+
+import SwiftUI
+import Combine
+
+struct AuditingFlightTimeView: View {
+    
+    @EnvironmentObject var dataController: DataController
+    
+    @State var auditor: Auditing
+    
+    var body: some View {
+        ScrollView {
+            VStack{
+                header
+                //PILOT HEADER
+                HStack{
+                    //PILOTS
+                    NameColumnView(title: "Pilots", crewLines: auditor.rows, hasGhost: true)
+                        .padding([.leading,.trailing])
+                    
+                    VStack(spacing:0){
+                        HStack(spacing:0){
+                            Text("Primary")
+                                .frame( maxWidth: .infinity, alignment: Alignment.center)
+                            Text("Secondary")
+                                .frame(maxWidth: .infinity, alignment: Alignment.center)
+                            Text("Instructor")
+                                .frame(maxWidth: .infinity, alignment: Alignment.center)
+                            Text("Evaluator")
+                                .frame(maxWidth: .infinity, alignment: Alignment.center)
+                            Text("Other")
+                                .frame( maxWidth: .infinity, alignment: Alignment.center)
+                            Text("Total")
+                                .frame(width: FlightTimeConstants.totalColumnWidth, alignment: Alignment.center)
+                            
+                        }
+                        .font(.subheadline)
+                        .frame(height: FlightTimeConstants.rowHeight)
+                        
+                        
+                        FlightTimeTotalsView(totals: auditor.ghostRow)
+                            .frame(height: FlightTimeConstants.rowHeight)
+                        
+                        ForEach((0..<auditor.rows.count), id: \.self){ index in
+                            CrewRowView(timeRow: $auditor.rows[index])
+                                .frame(height: FlightTimeConstants.rowHeight)
+                        }
+                        
+                        FlightTimeTotalsView(totals: auditor.totalRow)
+                            .frame(height: FlightTimeConstants.rowHeight)
+                    }
+                }
+                .background(Color.gray.opacity(0.2))
+                .overlay(
+                    RoundedRectangle(cornerRadius: 5)
+                        .stroke(Color.pblForegroundPrimary, lineWidth: 1)
+                )
+                .padding()
+                
+                VStack{
+                    
+                    ForEach(auditor.errors, id: \.self) { errorMessage in
+                        //   let index = pilotAuditor.errors.firstIndex(where: { errorMessage == $0 })!
+                        FadeInText(text:"⚠️ \(errorMessage)")
+                            .foregroundColor(.red)
+                            .frame(maxWidth:.infinity, alignment: .leading)
+                    }
+                    
+                }
+                .padding()
+                if auditor.dutyPosition == .loadmaster && auditor.rows.count > 1{
+                
+                    loadmasterStepper
+                }
+                Spacer()
+            }
+        }
+        .onDisappear {
+            auditor.sortie.objectWillChange.send()
+            auditor.save()
+            dataController.save()
+        }
+        .navigationBarTitle(Text("Splitting \(auditor.sortie.calculatedTotalFlightTimeFor781 ?? 0.0, specifier: "%1.1f") hours"))
+        
+    }
+    
+    var loadmasterStepper: some View {
+        HStack {
+            Spacer()
+            Text("Loadmasters required:")
+                .font(.pblBold(size: 17))
+                .foregroundColor(.pblForegroundSecondary)
+            Stepper("\(auditor.numLoadmastersRequired)", value: $auditor.numLoadmastersRequired, in: 1...Int16(min(3,auditor.rows.count)))
+                .font(.pblRegular(size: 18))
+                .foregroundColor(.pblForegroundSecondary)
+                .padding(.vertical, 8)
+                .padding(.horizontal)
+                .frame(width:150)
+                .background(
+                    RoundedRectangle(cornerRadius: .pblCornerRadius)
+                        .stroke(Color.pblForegroundPrimary)
+                )
+           
+        }
+        .padding()
+    }
+    
+    var header: some View {
+        HStack {
+            Spacer()
+            Button {
+                withAnimation(.bounce()){
+                    auditor.splitPrimaryEqually()
+                }
+                
+                
+            } label: {
+                HStack{
+                    Image(systemName: "clock")
+                    Text("Split Time Equally")
+                }.padding(.trailing)
+            }
+        }
+    }
+}
+
+import CoreData
+struct PilotFlightTimeView_Previews: PreviewProvider {
+    static let femaSortie: Sortie = {
+        let controller = DataController(inMemory: true)
+        let viewContext = controller.container.viewContext
+        SampleData.loadFEMAMission(viewContext: viewContext)
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Sortie")
+        var fetchedEntity = try? controller.container.viewContext.fetch(fetchRequest)
+        
+        fetchedEntity = Array((fetchedEntity?.prefix(4))!)
+        let sorties = fetchedEntity  as! [Sortie]
+        return sorties.randomElement()!
+    }()
+    static var previews: some View {
+        let auditor = PilotTimeAuditor(with: femaSortie)
+        
+        
+        
+        AuditingFlightTimeView(auditor: auditor)
+            .padding()
+            .previewLayout(.sizeThatFits)
+            .preferredColorScheme(.dark)
+    }
+}
+
